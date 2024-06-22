85c862751288d57dec1aa30b14efd53dc5bbba39
ALTR <chuan.khuna@gmail.com>
2024-06-22 14:55:44 +0700

    add more ts example

--- 02-higher-order.ts
+++ 02-higher-order.ts
@@ -3,5 +3,8 @@ let add = (x) => (y) => x + y;
 let addOne = (x) => x + 1;
 
 let numbers = [1, 2, 3, 4, 5];
+let numberObjs = [{ num: 1 }, { num: 2 }, { num: 3 }, { num: 4 }, { num: 5 }];
 
 numbers.map((x) => x ** 2).filter((x) => x > 4).reduce((a, b) => a + b, 0);
+
+numberObjs.map((x) => ({ num: x.num ** 2 })).filter((x) => x.num > 4).reduce((a, b) => a + b.num, 0);
